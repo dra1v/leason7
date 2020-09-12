de4bbdef310216327456e4b4b96d455060f11d3f
Evgenii <e.paprotsky@gmail.com>
2020-09-12 17:04:03 +0400

    Inicial commit

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@ public class Main {
     public static
     void main(String[] args) {
         Cat[] cats = {new Cat("Matroskin", 50), new Cat("Antowka", 50), new Cat("Canek", 25), new Cat("Valera", 5), new Cat("Kotik", 1)};
-        Plate plate = new Plate( 80);
+        Plate plate = new Plate( 100);
 
         for (Cat cat : cats) {
             cat.eat(plate);
             cat.info();
         }
+        plate.info();
+        plate.addFood(100);
+        plate.info();
     }
 }
 
